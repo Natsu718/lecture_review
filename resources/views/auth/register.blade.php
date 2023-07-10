5aa1e536c16801a52d98f32da38ec1687a510926
Natsu718 <o.natsumi1216@gmail.com>
2023-07-10 15:25:54 +0900

    ログイン機能

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,26 +4,33 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('氏名')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
         
         <!-- Department -->
-        <div>
-            
+        <div class="mt-4">
+            <x-input-label for="department" :value="__('学部を選択')" />
+            <select class="block mt-1 w-full" name="department">
+                @foreach($departments as $department)
+                    <option value="{{ $department->id }}">{{ $department->name }}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('department')" class="mt-2" />
         </div>
+        
         
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('メール')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('パスワード')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -35,7 +42,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('パスワード（確認用）')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"

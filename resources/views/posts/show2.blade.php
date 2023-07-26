914a5216eb78958681581bcd8838968dfb081196
Natsu718 <o.natsumi1216@gmail.com>
2023-07-26 15:46:19 +0900

    改行入れただけ

--- resources/views/posts/show2.blade.php
+++ resources/views/posts/show2.blade.php
@@ -5,28 +5,29 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <br>
-          <div>
-            <table>
-                <tr>
-                  <th width="100" align="center">評価方法</th>
-                  <th width="300" align="center">コメント</th>
-                  <th width="80" align="center">おすすめ度</th>
-                </tr>
-            </table>
+            <div>
+              <table>
+                  <tr>
+                    <th width="100" align="center">評価方法</th>
+                    <th width="300" align="center">コメント</th>
+                    <th width="80" align="center">おすすめ度</th>
+                  </tr>
+              </table>
+            </div>
+            <br>
+            @forelse($posts as $post)
+              <tr>
+                  <td width="100" align="center">{{ $post->grade->name}}</td>
+                  <td width="300" align="left">{{ $post->comment }}</td>
+                  <td width="80" align="center">{{ $post->review }}</td>
+                  
+              </tr>
+            @empty
+              <td>講義が見つかりません</td>
+            @endforelse
           </div>
           <br>
-          @forelse($posts as $post)
-            <tr>
-                <td width="100" align="center">{{ $post->grade->name}}</td>
-                <td width="300" align="left">{{ $post->comment }}</td>
-                <td width="80" align="center">{{ $post->review }}</td>
-                
-            </tr>
-          @empty
-            <td>講義が見つかりません</td>
-          @endforelse
         </div>
-        <br>
       </div>
     </div>
   </div>  

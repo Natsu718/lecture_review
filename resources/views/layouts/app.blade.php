f5f4ad28f9d8f3c30b364784b1952cf45e2ec21d
Natsu718 <o.natsumi1216@gmail.com>
2023-07-27 21:23:44 +0900

    .env変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>早稲田講義レビュー</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
